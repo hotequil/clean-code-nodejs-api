77f5d395c10ed622054d82c086e20f0cde8e0766
hotequil <hotequil@protonmail.com>
2022-01-06 22:48:27 -0300

    feat: create an interface for controllers

--- src/presentation/controllers/sign-up.ts
+++ src/presentation/controllers/sign-up.ts
@@ -3,8 +3,9 @@ import { StatusCode } from "status-code-enum";
 import { HttpRequest, HttpResponse } from "../protocols/http";
 import { MissingParamsError } from "../errors/missing-params-error";
 import { badRequest } from "../helpers/http-helper";
+import { Controller } from "../protocols/controller";
 
-export class SignUpController {
+export class SignUpController implements Controller {
     handle ({ body }: HttpRequest): HttpResponse {
         const requiredFields = ["name", "email", "password"];
 
