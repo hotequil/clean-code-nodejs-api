be01e29118a4b95e868c6bfc633e2cda6b4d5083
hotequil <hotequil@protonmail.com>
2022-01-06 21:24:02 -0300

    test: add test about email validation in SignUpController

--- src/presentation/controllers/sign-up.spec.ts
+++ src/presentation/controllers/sign-up.spec.ts
@@ -1,6 +1,10 @@
 import { SignUpController } from "./sign-up";
 
+let controller: SignUpController;
+
 describe("SignUpController", () => {
+    beforeEach(() => controller = new SignUpController());
+
     it("Should return code 400 when name is not provided", () => {
         const request = {
             body: {
@@ -10,10 +14,24 @@ describe("SignUpController", () => {
             }
         };
 
-        const controller = new SignUpController();
         const response = controller.handle(request);
 
         expect(response.statusCode).toBe(400);
         expect(response.body).toEqual(new Error("Missing param: name"));
+    });
+
+    it("Should return code 400 when email is not provided", () => {
+        const request = {
+            body: {
+                name: "name@name.name",
+                password: "passwordAndConfirmation",
+                passwordConfirmation: "passwordAndConfirmation"
+            }
+        };
+
+        const response = controller.handle(request);
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toEqual(new Error("Missing param: email"));
     });
 });
