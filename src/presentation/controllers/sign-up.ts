830776314a1adc7da0fd1d43c61522a834f07056
hotequil <hotequil@protonmail.com>
2022-01-06 22:38:50 -0300

    feat: add password as a required field

--- src/presentation/controllers/sign-up.ts
+++ src/presentation/controllers/sign-up.ts
@@ -6,7 +6,7 @@ import { badRequest } from "../helpers/http-helper";
 
 export class SignUpController {
     handle ({ body }: HttpRequest): HttpResponse {
-        const requiredFields = ["name", "email"];
+        const requiredFields = ["name", "email", "password"];
 
         for (const field of requiredFields) {
             if (!body[field]) return badRequest(new MissingParamsError(field));
