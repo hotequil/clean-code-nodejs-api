46b9e02dbd69f386b87a7df3e6f7bc13ccfaca1a
hotequil <hotequil@protonmail.com>
2022-01-12 20:43:39 -0300

    feat: add emailValidator dependency

--- src/presentation/controllers/sign-up.ts
+++ src/presentation/controllers/sign-up.ts
@@ -1,17 +1,28 @@
 import { StatusCode } from "status-code-enum";
 
 import { HttpRequest, HttpResponse } from "../protocols/http";
-import { MissingParamsError } from "../errors/missing-params-error";
+import { MissingParamsError } from "../errors/missing-params/missing-params-error";
 import { badRequest } from "../helpers/http-helper";
 import { Controller } from "../protocols/controller";
+import { EmailValidator } from "../protocols/email-validator";
+import { InvalidParamsError } from "../errors/invalid-params/invalid-params-error";
 
 export class SignUpController implements Controller {
+    private readonly emailValidator: EmailValidator;
+
+    constructor (emailValidator: EmailValidator) {
+        this.emailValidator = emailValidator;
+    }
+
     handle ({ body }: HttpRequest): HttpResponse {
         const requiredFields = ["name", "email", "password", "passwordConfirmation"];
 
         for (const field of requiredFields) {
             if (!body[field]) return badRequest(new MissingParamsError(field));
         }
+
+        if (!this.emailValidator.isValid(body.email))
+            return badRequest(new InvalidParamsError("email"));
 
         return { statusCode: StatusCode.SuccessOK, body: null };
     }
