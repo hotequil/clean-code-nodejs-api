9513953d48306157969b72c8047dd3fa67b70e85
hotequil <hotequil@protonmail.com>
2022-01-16 16:35:45 -0300

    test: verify with isEmail was called with a correct email

--- src/utils/adapters/email-validator/email-validator-adapter.spec.ts
+++ src/utils/adapters/email-validator/email-validator-adapter.spec.ts
@@ -6,23 +6,33 @@ jest.mock("validator", () => {
     return {
         isEmail: () => true
     }
-})
+});
 
 let adapter: EmailValidatorAdapter;
 
 describe("EmailValidatorAdapter", () => {
     beforeEach(() => adapter = new EmailValidatorAdapter());
 
-    it("Should return false if email is invalid when isValid was called", () => {
+    it("Should return false if isValid return false when was called", () => {
         jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
+
         const isValid = adapter.isValid("invalid@invalid.invalid");
 
         expect(isValid).toBe(false);
     });
 
-    it("Should return true if email is valid when isValid was called", () => {
+    it("Should return true if isValid return true when was called", () => {
         const isValid = adapter.isValid("valid@valid.valid");
 
         expect(isValid).toBe(true);
-    })
+    });
+
+    it("Should call isValid with a correct email when was called", () => {
+        const email = "email@email.email";
+        const isEmailSpy = jest.spyOn(validator, "isEmail");
+
+        adapter.isValid(email);
+
+        expect(isEmailSpy).toHaveBeenCalledWith(email);
+    });
 });
