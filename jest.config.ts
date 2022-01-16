460759aba326969a8220d1587c48b6691ee87d0f
hotequil <hotequil@protonmail.com>
2022-01-16 16:06:59 -0300

    feat: create email validator adapter and update jest files

--- jest.config.ts
+++ jest.config.ts
@@ -2,13 +2,16 @@ import { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
     roots: ["<rootDir>/src"],
-    collectCoverageFrom: ["<rootDir>/src/presentation/controllers/sign-up/sign-up.ts"],
     collectCoverage: true,
     coverageDirectory: "coverage",
     testEnvironment: "node",
     transform: {
         ".+\\.ts$": "ts-jest"
-    }
+    },
+    collectCoverageFrom: [
+        "<rootDir>/src/presentation/controllers/sign-up/sign-up.ts",
+        "<rootDir>/src/utils/adapters/email-validator/email-validator-adapter.ts"
+    ]
 };
 
 export default config;
