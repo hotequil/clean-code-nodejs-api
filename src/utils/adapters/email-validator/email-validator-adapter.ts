35e75bbbc85ab55744fe44922d270e6447a4936d
hotequil <hotequil@protonmail.com>
2022-01-16 16:23:29 -0300

    feat: use validator in EmailValidatorAdapter

--- src/utils/adapters/email-validator/email-validator-adapter.ts
+++ src/utils/adapters/email-validator/email-validator-adapter.ts
@@ -1,7 +1,9 @@
+import validator from "validator";
+
 import { EmailValidator } from "../../../presentation/protocols/email-validator";
 
 export class EmailValidatorAdapter implements EmailValidator {
     isValid (email: string): boolean {
-        return !!email;
+        return validator.isEmail(email);
     }
 }
