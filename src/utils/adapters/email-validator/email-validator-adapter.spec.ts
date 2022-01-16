8735b3920707e1e0062e4c6a8f52b7f6bdf25e62
hotequil <hotequil@protonmail.com>
2022-01-16 16:24:09 -0300

    test: verify valid email and use validator

--- src/utils/adapters/email-validator/email-validator-adapter.spec.ts
+++ src/utils/adapters/email-validator/email-validator-adapter.spec.ts
@@ -1,13 +1,28 @@
+import validator from "validator";
+
 import { EmailValidatorAdapter } from "./email-validator-adapter";
+
+jest.mock("validator", () => {
+    return {
+        isEmail: () => true
+    }
+})
 
 let adapter: EmailValidatorAdapter;
 
 describe("EmailValidatorAdapter", () => {
     beforeEach(() => adapter = new EmailValidatorAdapter());
 
-    it("Should return false if email is invalid when was called", () => {
-        const isValid = adapter.isValid("");
+    it("Should return false if email is invalid when isValid was called", () => {
+        jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
+        const isValid = adapter.isValid("invalid@invalid.invalid");
 
         expect(isValid).toBe(false);
     });
+
+    it("Should return true if email is valid when isValid was called", () => {
+        const isValid = adapter.isValid("valid@valid.valid");
+
+        expect(isValid).toBe(true);
+    })
 });
