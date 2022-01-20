02ee8bc01a415b6755d804ff7d3905edd7e2933d
hotequil <hotequil@protonmail.com>
2022-01-19 21:46:39 -0300

    feat: done repository for add account

--- src/data/use-cases/add-account/db-add-account.ts
+++ src/data/use-cases/add-account/db-add-account.ts
@@ -1,17 +1,18 @@
-import { AccountModel, AddAccount, AddAccountModel, Encrypter } from "./db-add-account-protocols";
+import { AccountModel, AddAccount, AddAccountModel, AddAccountRepository, Encrypter } from "./db-add-account-protocols";
 
 export class DbAddAccount implements AddAccount {
     private readonly encrypter: Encrypter;
+    private readonly addAccountRepository: AddAccountRepository;
 
-    constructor (encrypter: Encrypter) {
+    constructor (encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
         this.encrypter = encrypter;
+        this.addAccountRepository = addAccountRepository;
     }
 
     async add (account: AddAccountModel): Promise<AccountModel> {
         const password = await this.encrypter.encrypt(account.password);
+        const fakeAccount = await this.addAccountRepository.add({ ...account, password });
 
-        return await new Promise(resolve =>
-            resolve({ id: "", ...account, password })
-        );
+        return await new Promise(resolve => resolve({ ...account, ...fakeAccount }));
     }
 }
