bc2c9496270ed4baef356fd45b77bb9c7c7223bb
hotequil <hotequil@protonmail.com>
2022-01-20 21:32:42 -0300

    feat: create adapter for encrypt a value and add new library bcrypt

---
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -0,0 +1,11 @@
+import bcrypt from "bcrypt";
+
+import { Encrypter } from "../../data/protocols/encrypter";
+
+export class BcryptAdapter implements Encrypter {
+     constructor (private readonly SALT: number) {}
+
+    async encrypt (value: string): Promise<string> {
+        return await bcrypt.hash(value, this.SALT);
+    }
+}
