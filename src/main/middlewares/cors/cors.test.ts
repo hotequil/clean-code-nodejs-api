0af9306ee8105b013f831afbd03c8f9bdc67856f
hotequil <hotequil@protonmail.com>
2022-01-24 22:15:42 -0300

    test: enable cors in app

---
+++ src/main/middlewares/cors/cors.test.ts
@@ -0,0 +1,16 @@
+import request from "supertest";
+
+import app from "../../config/app";
+
+describe("CorsMiddleware", () => {
+    const route = "/test";
+
+    it("Should enable CORS in application", async () => {
+        app.get(route, (request, response) => response.send());
+
+        await request(app).get(route)
+                          .expect("access-control-allow-origin", "*")
+                          .expect("access-control-allow-methods", "*")
+                          .expect("access-control-allow-headers", "*");
+    });
+});
