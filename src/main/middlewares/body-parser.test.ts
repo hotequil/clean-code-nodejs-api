f739aebdb395dc89261c01b8c66ab48b2bfc7685
hotequil <hotequil@protonmail.com>
2022-01-24 21:42:11 -0300

    feat: create a server and middleware file to return json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,15 @@
+import request from "supertest";
+
+import app from "../config/app";
+
+describe("BodyParserMiddleware", () => {
+    const route = "/test";
+
+    it(`Should parser a request to JSON at route ${route}`, async () => {
+        const body = { key: "value" };
+
+        app.post(route, (request, response) => response.send(request.body));
+
+        await request(app).post(route).send(body).expect(body);
+    });
+});
---
+++ src/main/server.ts
@@ -0,0 +1,5 @@
+import app from "./config/app";
+
+const PORT = 4000;
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
