afb188eb4723d3f1bd0bd52bd8a8086a29915b9d
hotequil <hotequil@protonmail.com>
2022-01-26 20:17:55 -0300

    refactor: add a catch on init db

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,9 +1,11 @@
 import env from "./config/env";
 import { MongodbHelper } from "../infra/db/mongodb/helpers/mongodb-helper";
 
-void MongodbHelper.connect().then(async () => {
-    const appImported = await import("./config/app");
-    const app = appImported.default;
+void MongodbHelper.connect()
+                  .then(async () => {
+                      const appImported = await import("./config/app");
+                      const app = appImported.default;
 
-    app.listen(env.PORT, () => console.log(`Server running at port ${env.PORT}`));
-});
+                      app.listen(env.PORT, () => console.log(`Server running at port ${env.PORT}`));
+                  })
+                  .catch(console.error);
