2570ffb72b1eb6f9834db3ff3b5d668b2b4bb651
hotequil <hotequil@protonmail.com>
2022-01-28 20:18:05 -0300

    feat: use LogDecorator

--- src/main/factories/sign-up.ts
+++ src/main/factories/sign-up.ts
@@ -3,13 +3,16 @@ import { EmailValidatorAdapter } from "../../utils/adapters/email-validator/emai
 import { DbAddAccount } from "../../data/use-cases/add-account/db-add-account";
 import { Account } from "../../infra/db/mongodb/account-repository/account";
 import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
+import { LogDecorator } from "../decorators/log/log";
+import { Controller } from "../../presentation/protocols";
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
     const SALT = 12;
     const emailValidator = new EmailValidatorAdapter();
     const account = new Account();
     const encrypter = new BcryptAdapter(SALT);
     const dbAddAccount = new DbAddAccount(encrypter, account);
+    const signUpController = new SignUpController(emailValidator, dbAddAccount);
 
-    return new SignUpController(emailValidator, dbAddAccount);
+    return new LogDecorator(signUpController);
 };
