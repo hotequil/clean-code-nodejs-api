0993aec1df6204be032a4ab85a5d66a7dcf1ceeb
hotequil <hotequil@protonmail.com>
2022-01-28 21:21:11 -0300

    feat: check 500 code and save log

--- src/main/decorators/log/log.ts
+++ src/main/decorators/log/log.ts
@@ -1,14 +1,15 @@
 import StatusCode from "status-code-enum";
 
 import { Controller, HttpRequest, HttpResponse } from "../../../presentation/protocols";
+import { LogErrorRepository } from "../../../data/protocols/log-error-repository";
 
 export class LogDecorator implements Controller {
-    constructor (private readonly controller: Controller) {}
+    constructor (private readonly controller: Controller, private readonly logErrorRepository: LogErrorRepository) {}
 
     async handle (request: HttpRequest): Promise<HttpResponse> {
         const response = await this.controller.handle(request);
 
-        if (response.statusCode === StatusCode.ServerErrorInternal) console.error(response);
+        if (response.statusCode === StatusCode.ServerErrorInternal) await this.logErrorRepository.log(response.body.stack);
 
         return response;
     }
