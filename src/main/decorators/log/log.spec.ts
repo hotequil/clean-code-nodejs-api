b7ab9853309ce04ea60c91068565c4a708e1d9bb
hotequil <hotequil@protonmail.com>
2022-01-28 21:21:29 -0300

    test: check 500 code and save log

--- src/main/decorators/log/log.spec.ts
+++ src/main/decorators/log/log.spec.ts
@@ -2,6 +2,8 @@ import StatusCode from "status-code-enum";
 
 import { Controller, HttpRequest, HttpResponse } from "../../../presentation/protocols";
 import { LogDecorator } from "./log";
+import { serverError } from "../../../presentation/helpers/http-helper";
+import { LogErrorRepository } from "../../../data/protocols/log-error-repository";
 
 const RESPONSE_MOCK = {
     statusCode: StatusCode.SuccessOK,
@@ -18,13 +20,23 @@ class ControllerStub implements Controller {
     }
 }
 
+class LogErrorRepositoryStub implements LogErrorRepository {
+    async log (stack: string): Promise<void> {
+        console.log(stack);
+
+        return await new Promise(resolve => resolve());
+    }
+}
+
 describe("LogDecorator", () => {
     let logDecorator: LogDecorator;
     let controllerStub: ControllerStub;
+    let logErrorRepositoryStub: LogErrorRepository;
 
     beforeEach(() => {
         controllerStub = new ControllerStub();
-        logDecorator = new LogDecorator(controllerStub);
+        logErrorRepositoryStub = new LogErrorRepositoryStub();
+        logDecorator = new LogDecorator(controllerStub, logErrorRepositoryStub);
     });
 
     it("Should call handle and send data when was called", async () => {
@@ -52,5 +64,23 @@ describe("LogDecorator", () => {
         const httpResponse = await logDecorator.handle(request);
 
         expect(httpResponse).toBe(RESPONSE_MOCK);
+    });
+
+    it(`Should get a stack trace when throw an error with code ${StatusCode.ServerErrorInternal}`, async () => {
+        const logSpy = jest.spyOn(logErrorRepositoryStub, "log");
+        const httpRequest: HttpRequest = { body: { name: "name" } };
+        const error = new Error();
+        const stack = "Error from server";
+
+        error.stack = stack;
+
+        const errorFromServer = serverError(error);
+
+        jest.spyOn(controllerStub, "handle")
+            .mockImplementationOnce(async () => await new Promise(resolve => resolve(errorFromServer)));
+
+        await logDecorator.handle(httpRequest);
+
+        expect(logSpy).toHaveBeenCalledWith(stack);
     });
 });
