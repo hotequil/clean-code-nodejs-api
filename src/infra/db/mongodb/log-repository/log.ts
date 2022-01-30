0f59260c29d4d2d6d117f8e9598241f2fc1eb34c
hotequil <hotequil@protonmail.com>
2022-01-30 14:05:44 -0300

    feat: create repository for LogMongoRepository

---
+++ src/infra/db/mongodb/log-repository/log.ts
@@ -0,0 +1,10 @@
+import { LogErrorRepository } from "../../../../data/protocols/log-error-repository";
+import { MongodbHelper } from "../helpers/mongodb-helper";
+
+export class LogMongoRepository implements LogErrorRepository {
+    async logError (stack: string): Promise<void> {
+        const errors = await MongodbHelper.collection("errors");
+
+        await errors.insertOne({ stack, date: new Date() });
+    }
+}
