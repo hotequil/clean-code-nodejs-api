b2ac1a06a53c1d9779b34c08d3d98d683c6f4e69
hotequil <hotequil@protonmail.com>
2022-01-30 14:05:58 -0300

    test: create repository for LogMongoRepository

---
+++ src/infra/db/mongodb/log-repository/log.test.ts
@@ -0,0 +1,30 @@
+import { Collection } from "mongodb";
+
+import { MongodbHelper } from "../helpers/mongodb-helper";
+import { LogMongoRepository } from "./log";
+
+describe("LogMongoRepository", () => {
+    let repository: LogMongoRepository;
+    let errors: Collection;
+
+    beforeAll(async () => await MongodbHelper.connect());
+    afterAll(async () => await MongodbHelper.disconnect());
+
+    beforeEach(async () => {
+        errors = await MongodbHelper.collection("errors");
+
+        await errors.deleteMany({});
+
+        repository = new LogMongoRepository();
+    });
+
+    it("Should save an error log when was called", async () => {
+        let count = 0;
+
+        await repository.logError("error");
+
+        count = await errors.countDocuments();
+
+        expect(count).toBeTruthy();
+    });
+});
