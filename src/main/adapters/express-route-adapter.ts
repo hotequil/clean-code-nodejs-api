f1d783c627b05c68da6545909b0509d8a830ec87
hotequil <hotequil@protonmail.com>
2022-01-30 14:33:37 -0300

    feat: add only message error on save

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from "express";
+import StatusCode from "status-code-enum";
 
 import { Controller, HttpRequest } from "../../presentation/protocols";
 
 export const adaptRoute = (controller: Controller) => {
     return async (request: Request, response: Response) => {
         const httpRequest: HttpRequest = { body: request.body };
-        const httpResponse = await controller.handle(httpRequest);
+        const { statusCode, body } = await controller.handle(httpRequest);
 
-        response.status(httpResponse.statusCode).json(httpResponse.body);
+        response.status(statusCode);
+
+        if (statusCode === StatusCode.ServerErrorInternal) response.json({ error: body.message });
+        else response.json(body);
     }
 };
