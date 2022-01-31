64dcb7c0b9c38200cb55983e590d99330382e4f6
hotequil <hotequil@protonmail.com>
2022-01-30 21:57:41 -0300

    feat: verify if email is correct

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,11 @@
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
-import { badRequest } from "../../helpers/http-helper";
-import { MissingParamsError } from "../../errors";
+import { badRequest, success } from "../../helpers/http-helper";
+import { InvalidParamsError, MissingParamsError } from "../../errors";
+import { EmailValidator } from "../../protocols/email-validator";
 
 export class LoginController implements Controller {
+    constructor (private readonly emailValidator: EmailValidator) {}
+
     async handle (request: HttpRequest): Promise<HttpResponse> {
         return await new Promise(resolve => {
             const { password, email } = request.body;
@@ -10,8 +13,10 @@ export class LoginController implements Controller {
 
             if (!email) error = new MissingParamsError("email");
             else if (!password) error = new MissingParamsError("password");
+            else if (!this.emailValidator.isValid(email)) error = new InvalidParamsError("email");
 
             if (error) resolve(badRequest(error));
+            else resolve(success(null));
         });
     }
 }
