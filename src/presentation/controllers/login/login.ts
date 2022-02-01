15d52f19bf9d3ab82a36b9c39a5c6d916596e498
hotequil <hotequil@protonmail.com>
2022-01-31 21:20:18 -0300

    feat: verify exceptions

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
-import { badRequest, success } from "../../helpers/http-helper";
+import { badRequest, serverError, success } from "../../helpers/http-helper";
 import { InvalidParamsError, MissingParamsError } from "../../errors";
 import { EmailValidator } from "../../protocols/email-validator";
 
@@ -8,15 +8,19 @@ export class LoginController implements Controller {
 
     async handle (request: HttpRequest): Promise<HttpResponse> {
         return await new Promise(resolve => {
-            const { password, email } = request.body;
-            let error: MissingParamsError|null = null;
+            try {
+                const { password, email } = request.body;
+                let error: MissingParamsError|null = null;
 
-            if (!email) error = new MissingParamsError("email");
-            else if (!password) error = new MissingParamsError("password");
-            else if (!this.emailValidator.isValid(email)) error = new InvalidParamsError("email");
+                if (!email) error = new MissingParamsError("email");
+                else if (!password) error = new MissingParamsError("password");
+                else if (!this.emailValidator.isValid(email)) error = new InvalidParamsError("email");
 
-            if (error) resolve(badRequest(error));
-            else resolve(success(null));
+                if (error) resolve(badRequest(error));
+                else resolve(success(null));
+            } catch (error) {
+                resolve(serverError(error as Error));
+            }
         });
     }
 }
