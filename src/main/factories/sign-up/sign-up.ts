6956cb7bab29b3f2be01d25927fe61983726e9ec
hotequil <hotequil@protonmail.com>
2022-02-06 17:39:38 -0300

    feat: add validation compisite to SignUpController

--- src/main/factories/sign-up/sign-up.ts
+++ src/main/factories/sign-up/sign-up.ts
@@ -1,11 +1,11 @@
-import { SignUpController } from "../../presentation/controllers/sign-up/sign-up";
-import { EmailValidatorAdapter } from "../../utils/adapters/email-validator/email-validator-adapter";
-import { DbAddAccount } from "../../data/use-cases/add-account/db-add-account";
-import { Account } from "../../infra/db/mongodb/account-repository/account";
-import { BcryptAdapter } from "../../infra/criptography/bcrypt-adapter";
-import { LogDecorator } from "../decorators/log/log";
-import { Controller } from "../../presentation/protocols";
-import { LogMongoRepository } from "../../infra/db/mongodb/log-repository/log";
+import { SignUpController } from "../../../presentation/controllers/sign-up/sign-up";
+import { EmailValidatorAdapter } from "../../../utils/adapters/email-validator/email-validator-adapter";
+import { DbAddAccount } from "../../../data/use-cases/add-account/db-add-account";
+import { Account } from "../../../infra/db/mongodb/account-repository/account";
+import { BcryptAdapter } from "../../../infra/criptography/bcrypt-adapter";
+import { LogDecorator } from "../../decorators/log/log";
+import { Controller } from "../../../presentation/protocols";
+import { LogMongoRepository } from "../../../infra/db/mongodb/log-repository/log";
 import { makeSignUpValidationComposite } from "./sign-up-validation-composite";
 
 export const makeSignUpController = (): Controller => {
