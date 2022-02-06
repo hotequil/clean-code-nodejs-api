d1692c83657664c835d409947e65b760f7a64fa3
hotequil <hotequil@protonmail.com>
2022-02-06 17:39:26 -0300

    refactor: change file directory

--- src/main/routes/sign-up-routes.ts
+++ src/main/routes/sign-up-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
 import { adaptRoute } from "../adapters/express-route-adapter";
-import { makeSignUpController } from "../factories/sign-up";
+import { makeSignUpController } from "../factories/sign-up/sign-up";
 
 export default (router: Router): void => {
     router.post("/sign-up", adaptRoute(makeSignUpController()));
