4726f77e3dc3e1c400291e29025c913d3c9c4a2e
hotequil <hotequil@protonmail.com>
2022-02-13 10:48:49 -0300

    refactor: set email-validation in sign-up-validation

--- src/main/factories/sign-up/sign-up-validation-composite.spec.ts
+++ src/main/factories/sign-up/sign-up-validation-composite.spec.ts
@@ -2,8 +2,18 @@ import { makeSignUpValidationComposite } from "./sign-up-validation-composite";
 import { ValidationComposite } from "../../../presentation/validators/validation-composite";
 import { RequiredFieldValidation } from "../../../presentation/validators/required-field-validation";
 import { CompareFieldsValidation } from "../../../presentation/validators/compare-fields-validation";
+import { EmailValidation } from "../../../presentation/validators/email-validation";
+import { EmailValidator } from "../../../presentation/protocols/email-validator";
 
 jest.mock("../../../presentation/validators/validation-composite");
+
+class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+        console.log(email);
+
+        return true;
+    }
+}
 
 describe("SignUpValidationComposite", () => {
     it("Should call ValidationComposite with all validations when was called", () => {
@@ -14,7 +24,8 @@ describe("SignUpValidationComposite", () => {
             new RequiredFieldValidation("email"),
             new RequiredFieldValidation("password"),
             new RequiredFieldValidation("passwordConfirmation"),
-            new CompareFieldsValidation("password", "passwordConfirmation")
+            new CompareFieldsValidation("password", "passwordConfirmation"),
+            new EmailValidation("email", new EmailValidatorStub())
         ]);
     });
 });
--- src/main/factories/sign-up/sign-up-validation-composite.ts
+++ src/main/factories/sign-up/sign-up-validation-composite.ts
@@ -1,11 +1,14 @@
 import { ValidationComposite } from "../../../presentation/validators/validation-composite";
 import { RequiredFieldValidation } from "../../../presentation/validators/required-field-validation";
 import { CompareFieldsValidation } from "../../../presentation/validators/compare-fields-validation";
+import { EmailValidation } from "../../../presentation/validators/email-validation";
+import { EmailValidatorAdapter } from "../../../utils/adapters/email-validator/email-validator-adapter";
 
 export const makeSignUpValidationComposite = (): ValidationComposite => new ValidationComposite([
     new RequiredFieldValidation("name"),
     new RequiredFieldValidation("email"),
     new RequiredFieldValidation("password"),
     new RequiredFieldValidation("passwordConfirmation"),
-    new CompareFieldsValidation("password", "passwordConfirmation")
+    new CompareFieldsValidation("password", "passwordConfirmation"),
+    new EmailValidation("email", new EmailValidatorAdapter())
 ]);
