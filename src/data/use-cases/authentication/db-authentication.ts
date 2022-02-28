9ab82886acd283c2de3bd44cc303ef91efb2ebd6
hotequil <hotequil@protonmail.com>
2022-02-28 14:25:03 -0300

    feat: add a HashComparer in DbAuthentication

--- src/data/use-cases/authentication/db-authentication.ts
+++ src/data/use-cases/authentication/db-authentication.ts
@@ -1,14 +1,20 @@
 import { Authentication, AuthenticationModel } from "../../../domain/use-cases/authentication";
 import { LoadAccountByEmailRepository } from "../../protocols/db/load-account-by-email-repository";
+import { HashComparer } from "../../protocols/criptography/hash-comparer";
 
 export class DbAuthentication implements Authentication {
-    constructor (private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository) {}
+    constructor (
+        private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+        private readonly hashComparer: HashComparer
+    ) {}
 
     async auth (model: AuthenticationModel): Promise<string|null> {
         const account = await this.loadAccountByEmailRepository.load(model.email);
 
         if (!account) return null;
 
-        return "TOKEN";
+        await this.hashComparer.compare(account.password, model.password);
+
+        return null;
     }
 }
