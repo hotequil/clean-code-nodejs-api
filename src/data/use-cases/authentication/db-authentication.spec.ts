071e5297a084376e76d078d2c6e2491a566661d3
hotequil <hotequil@protonmail.com>
2022-02-28 14:25:50 -0300

    test: verify if HashComparer received passwords

--- src/data/use-cases/authentication/db-authentication.spec.ts
+++ src/data/use-cases/authentication/db-authentication.spec.ts
@@ -2,25 +2,38 @@ import { Authentication, AuthenticationModel } from "../../../domain/use-cases/a
 import { AccountModel } from "../../../domain/models/account";
 import { LoadAccountByEmailRepository } from "../../protocols/db/load-account-by-email-repository";
 import { DbAuthentication } from "./db-authentication";
+import { HashComparer } from "../../protocols/criptography/hash-comparer";
 
 const DEFAULT_EMAIL = "email@email.email";
-const createAuthModel = (): AuthenticationModel => ({ email: DEFAULT_EMAIL, password: "1a2b3c4d" });
+const DEFAULT_PASSWORD = "1a2b3c4d";
+const ACCOUNT_PASSWORD = "password";
+const createAuthModel = (): AuthenticationModel => ({ email: DEFAULT_EMAIL, password: DEFAULT_PASSWORD });
 
 class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
     async load (email: string): Promise<AccountModel> {
-        const account = { email, id: "id", name: "name", password: "password" };
+        const account = { email, id: "id", name: "name", password: ACCOUNT_PASSWORD };
 
         return await new Promise(resolve => resolve(account));
+    }
+}
+
+class HashComparerStub implements HashComparer {
+    async compare (value: string, hash: string): Promise<boolean> {
+        console.log(value, hash);
+
+        return await new Promise(resolve => resolve(true));
     }
 }
 
 describe("DbAuthentication", () => {
     let db: Authentication;
     let loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository;
+    let hashComparerStub: HashComparer;
 
     beforeEach(() => {
         loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub();
-        db = new DbAuthentication(loadAccountByEmailRepositoryStub);
+        hashComparerStub = new HashComparerStub();
+        db = new DbAuthentication(loadAccountByEmailRepositoryStub, hashComparerStub);
     });
 
     it("Should call LoadAccountByEmailRepository with correct email when was called", async () => {
@@ -46,5 +59,13 @@ describe("DbAuthentication", () => {
         const response = await db.auth(createAuthModel());
 
         expect(response).toBeNull();
+    });
+
+    it("Should call HashComparer with correct passwords when was called", async () => {
+        const compareSpy = jest.spyOn(hashComparerStub, "compare");
+
+        await db.auth(createAuthModel());
+
+        expect(compareSpy).toHaveBeenCalledWith(ACCOUNT_PASSWORD, DEFAULT_PASSWORD);
     });
 });
