5c4a597a52c1b3d1554b70761e41e406244fd0d0
hotequil <hotequil@protonmail.com>
2022-02-27 21:52:31 -0300

    test: verify if ValidationComposite throw an error

---
+++ src/presentation/validators/validation-composite.spec.ts
@@ -0,0 +1,29 @@
+import { ValidationComposite } from "./validation-composite";
+import { MissingParamsError } from "../errors";
+import { Validation } from "./validation";
+import { AnyObject } from "../../utils/helpers";
+
+let validation: ValidationComposite;
+const FIELD = "id";
+
+class ValidationStub implements Validation {
+    validate (value: AnyObject): MissingParamsError|null {
+        console.log(value);
+
+        return null;
+    }
+}
+
+describe("ValidationComposite", () => {
+    beforeEach(() => validation = new ValidationComposite([new ValidationStub()]));
+
+    it("Should return an error if any validate throw when was called", () => {
+        const value = new MissingParamsError(FIELD);
+
+        jest.spyOn(validation, "validate").mockReturnValueOnce(value);
+
+        const response = validation.validate({});
+
+        expect(response).toEqual(value);
+    });
+});
