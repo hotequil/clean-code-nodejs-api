e717c9a3dbeb06da20847174b8ff804c19b57186
hotequil <hotequil@protonmail.com>
2022-03-04 21:36:24 -0300

    feat: create method compare and use interface HashComparer

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -1,11 +1,16 @@
 import bcrypt from "bcrypt";
 
 import { Hasher } from "../../data/protocols/criptography/hasher";
+import { HashComparer } from "../../data/protocols/criptography/hash-comparer";
 
-export class BcryptAdapter implements Hasher {
+export class BcryptAdapter implements Hasher, HashComparer {
      constructor (private readonly SALT: number) {}
 
     async hash (value: string): Promise<string> {
         return await bcrypt.hash(value, this.SALT);
+    }
+
+    async compare (value: string, hash: string): Promise<boolean> {
+         return await bcrypt.compare(value, hash);
     }
 }
