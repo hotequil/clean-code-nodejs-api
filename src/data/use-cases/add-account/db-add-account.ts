7c9baaec61a8bacf455edfccb9a6fa54fe55e078
hotequil <hotequil@protonmail.com>
2022-03-14 20:49:07 -0300

    refactor: change constructor dependecy injection

--- src/data/use-cases/add-account/db-add-account.ts
+++ src/data/use-cases/add-account/db-add-account.ts
@@ -1,13 +1,7 @@
 import { AccountModel, AddAccount, AddAccountModel, AddAccountRepository, Hasher } from "./db-add-account-protocols";
 
 export class DbAddAccount implements AddAccount {
-    private readonly hasher: Hasher;
-    private readonly addAccountRepository: AddAccountRepository;
-
-    constructor (hasher: Hasher, addAccountRepository: AddAccountRepository) {
-        this.hasher = hasher;
-        this.addAccountRepository = addAccountRepository;
-    }
+    constructor (private readonly hasher: Hasher, private readonly addAccountRepository: AddAccountRepository) {}
 
     async add (account: AddAccountModel): Promise<AccountModel> {
         const password = await this.hasher.hash(account.password);
--- src/infra/criptography/bcrypt/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt/bcrypt-adapter.ts
@@ -4,7 +4,7 @@ import { Hasher } from "../../../data/protocols/criptography/hasher";
 import { HashComparer } from "../../../data/protocols/criptography/hash-comparer";
 
 export class BcryptAdapter implements Hasher, HashComparer {
-     constructor (private readonly SALT: number) {}
+    constructor (private readonly SALT: number) {}
 
     async hash (value: string): Promise<string> {
         return await bcrypt.hash(value, this.SALT);
