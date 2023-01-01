b9b491283a464bc1e4ebcd21462a9be641750f57
hotequil <hotequil@protonmail.com>
2023-01-01 10:48:07 -0300

    feat: set Authentication in sign-up factory

--- src/main/factories/sign-up/sign-up-factory.ts
+++ src/main/factories/sign-up/sign-up-factory.ts
@@ -6,14 +6,19 @@ import { LogDecorator } from "../../decorators/log/log-controller-decorator";
 import { Controller } from "../../../presentation/protocols";
 import { LogMongoRepository } from "../../../infra/db/mongodb/log-repository/log-mongo-repository"
 import { makeSignUpValidationComposite } from "./sign-up-validation-factory";
+import { JwtAdapter } from "../../../infra/criptography/jwt/jwt-adapter";
+import env from "../../config/env";
+import { DbAuthentication } from "../../../data/use-cases/authentication/db-authentication";
 
 export const makeSignUpController = (): Controller => {
     const SALT = 12;
     const account = new AccountMongoRepository();
     const hasher = new BcryptAdapter(SALT);
     const dbAddAccount = new DbAddAccount(hasher, account);
+    const jwtAdapter = new JwtAdapter(env.JWT_SECRET);
+    const dbAuthentication = new DbAuthentication(account, hasher, jwtAdapter, account);
     const validationComposite = makeSignUpValidationComposite();
-    const signUpController = new SignUpController(dbAddAccount, validationComposite);
+    const signUpController = new SignUpController(dbAddAccount, validationComposite, dbAuthentication);
     const logMongoRepository = new LogMongoRepository();
 
     return new LogDecorator(signUpController, logMongoRepository);
