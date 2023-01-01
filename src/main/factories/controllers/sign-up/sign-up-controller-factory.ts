85d07fd69054b7918bf4fefdab13ab65cc825aa3
hotequil <hotequil@protonmail.com>
2023-01-01 15:53:18 -0300

    refactor: used factories for make controllers

---
+++ src/main/factories/controllers/sign-up/sign-up-controller-factory.ts
@@ -0,0 +1,9 @@
+import { SignUpController } from "../../../../presentation/controllers/sign-up/sign-up-controller";
+import { Controller } from "../../../../presentation/protocols";
+import { makeSignUpValidationComposite } from "./sign-up-validation-factory";
+import { makeDbAuthentication } from "../../use-cases/authentication/db-authentication-factory";
+import { makeDbAddAccount } from "../../use-cases/add-account/db-add-account-factory";
+import { makeLogDecorator } from "../../use-cases/decorators/log-decorator-factory";
+
+export const makeSignUpController = (): Controller =>
+    makeLogDecorator(new SignUpController(makeDbAddAccount(), makeSignUpValidationComposite(), makeDbAuthentication()));
