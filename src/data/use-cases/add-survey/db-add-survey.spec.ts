5f85d456aa83ef4e5ad52ea966cd8d2d36ec25f1
hotequil <hotequil@protonmail.com>
2023-01-03 19:47:22 -0300

    test: verified if AddSurveyRepository is called with correct values in DbAddSurvey

---
+++ src/data/use-cases/add-survey/db-add-survey.spec.ts
@@ -0,0 +1,38 @@
+import { DbAddSurvey } from "./db-add-survey";
+import { AddSurveyModel, AddSurveyRepository } from "./db-add-survey-protocols";
+
+let db: DbAddSurvey
+let addSurveyRepositoryStub: AddSurveyRepository
+
+const makeAddSurveyModel = (): AddSurveyModel => ({
+    question: "question",
+    answers: [
+        { image: "image", answer: "answer" },
+        { image: "image", answer: "answer" },
+        { image: "image", answer: "answer" },
+    ]
+})
+
+class AddSurveyRepositoryStub implements AddSurveyRepository{
+    async add(model: AddSurveyModel): Promise<null>{
+        console.log(model)
+
+        return await new Promise(resolve => resolve(null))
+    }
+}
+
+describe(DbAddSurvey.name, () => {
+    beforeEach(() => {
+        addSurveyRepositoryStub = new AddSurveyRepositoryStub()
+        db = new DbAddSurvey(addSurveyRepositoryStub)
+    })
+
+    it("Should call AddSurveyRepository with correct values", async () => {
+        const repositorySpy = jest.spyOn(addSurveyRepositoryStub, "add")
+        const model = makeAddSurveyModel()
+
+        await db.add(model)
+
+        expect(repositorySpy).toHaveBeenCalledWith(model)
+    })
+})
