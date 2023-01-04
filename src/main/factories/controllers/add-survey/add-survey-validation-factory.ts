9b5f65b4081d227b1bd9bcf9bc9e027b3b2742d7
hotequil <hotequil@protonmail.com>
2023-01-04 20:43:43 -0300

    feat: created AddSUrveyValidationComposite and AddSurveyController

---
+++ src/main/factories/controllers/add-survey/add-survey-validation-factory.ts
@@ -0,0 +1,6 @@
+import { RequiredFieldValidation, ValidationComposite } from "../../../../validation/validators";
+
+export const makeAddSurveyValidationComposite = (): ValidationComposite => new ValidationComposite([
+    new RequiredFieldValidation("question"),
+    new RequiredFieldValidation("answers"),
+]);
