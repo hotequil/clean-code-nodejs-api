47a19477a958a4b5325828e9d6221e12c0f79a1e
hotequil <hotequil@protonmail.com>
2023-01-05 21:07:36 -0300

    test: done verification id LoadAccountByToken is called with correct params

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -2,15 +2,53 @@ import { AuthMiddleware } from "./auth-middleware";
 import StatusCode from "status-code-enum";
 import { forbidden } from "../helpers/http-helper";
 import { AccessDeniedError } from "../errors";
+import { HttpRequest } from "../protocols";
+import { Header } from "../../utils/enums";
+import { LoadAccountByToken } from "../../domain/use-cases/load-account-by-token";
+import { AccountModel } from "../../domain/models/account";
 
 let middleware: AuthMiddleware;
+let loadAccountByToken: LoadAccountByToken;
+const FAKE_TOKEN = "token"
+
+const makeFakeAccountModel = (): AccountModel => ({
+    email: "email@test.com",
+    name: "name",
+    password: "password",
+    id: "id",
+    accessToken: "accessToken",
+})
+
+class LoadAccountByTokenStub implements LoadAccountByToken{
+    async loadByToken(token: string): Promise<AccountModel | null>{
+        console.log(token)
+
+        return await new Promise(resolve => resolve(makeFakeAccountModel()))
+    }
+}
 
 describe(AuthMiddleware.name, () => {
-    beforeEach(() => middleware = new AuthMiddleware())
+    beforeEach(() => {
+        loadAccountByToken = new LoadAccountByTokenStub()
+        middleware = new AuthMiddleware(loadAccountByToken)
+    })
 
-    it(`Should return code ${StatusCode.ClientErrorForbidden} if has no x-access-token in request headers`, async () => {
+    it(`Should return code ${StatusCode.ClientErrorForbidden} if has no ${Header.X_ACCESS_TOKEN} in request headers`, async () => {
         const response = await middleware.handle({})
 
         expect(response).toEqual(forbidden(new AccessDeniedError()))
     })
+
+    it("Should call LoadAccountByToken with correct token", async () => {
+        const loadAccountByTokenSpy = jest.spyOn(loadAccountByToken, "loadByToken")
+        const request: HttpRequest = {
+            headers: {
+                [Header.X_ACCESS_TOKEN]: FAKE_TOKEN
+            }
+        }
+
+        await middleware.handle(request)
+
+        expect(loadAccountByTokenSpy).toHaveBeenCalledWith(FAKE_TOKEN)
+    });
 });
