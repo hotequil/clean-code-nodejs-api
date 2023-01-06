108fa652286e5bcf075109ba5b186c67f7ce61bc
hotequil <hotequil@protonmail.com>
2023-01-05 21:03:43 -0300

    feat: added dependency of LoadAccountByToken and used loadByToken method in AuthMiddleware

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -2,12 +2,16 @@ import { Middleware } from "../protocols/middleware";
 import { HttpRequest, HttpResponse } from "../protocols";
 import { AccessDeniedError } from "../errors";
 import { forbidden } from "../helpers/http-helper";
+import { LoadAccountByToken } from "../../domain/use-cases/load-account-by-token";
+import { Header } from "../../utils/enums";
 
 export class AuthMiddleware implements Middleware{
-    async handle(request: HttpRequest): Promise<HttpResponse>{
-        const token = request?.headers?.["x-access-token"]
+    constructor(private readonly loadAccountByToken: LoadAccountByToken){}
 
-        console.log(token)
+    async handle(request: HttpRequest): Promise<HttpResponse>{
+        const token = request?.headers?.[Header.X_ACCESS_TOKEN]
+
+        if(token) await this.loadAccountByToken.loadByToken(token)
 
         return forbidden(new AccessDeniedError())
     }
