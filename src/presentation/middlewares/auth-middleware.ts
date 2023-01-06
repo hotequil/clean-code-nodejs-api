aa8c4e66192f1ae445138ea5a96b3b33aab0a7a4
hotequil <hotequil@protonmail.com>
2023-01-05 22:09:36 -0300

    feat: added role param in AuthMiddleware

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -3,17 +3,17 @@ import { HttpRequest, HttpResponse } from "../protocols";
 import { AccessDeniedError } from "../errors";
 import { forbidden, serverError, success } from "../helpers/http-helper";
 import { LoadAccountByToken } from "../../domain/use-cases/load-account-by-token";
-import { Header } from "../../utils/enums";
+import { AccountType, Header } from "../../utils/enums";
 
 export class AuthMiddleware implements Middleware{
-    constructor(private readonly loadAccountByToken: LoadAccountByToken){}
+    constructor(private readonly loadAccountByToken: LoadAccountByToken, private readonly role?: AccountType){}
 
     async handle(request: HttpRequest): Promise<HttpResponse>{
         try{
             const token = request?.headers?.[Header.X_ACCESS_TOKEN]
 
             if(token) {
-                const account = await this.loadAccountByToken.loadByToken(token)
+                const account = await this.loadAccountByToken.loadByToken(token, this.role)
 
                 if(account) return success({ accountId: account.id })
             }
