8ef41446c8ab89bf85c4d60a95fa986ab90af942
hotequil <hotequil@protonmail.com>
2023-01-05 21:00:25 -0300

    feat: created LoadAccountByToken protocol

---
+++ src/domain/use-cases/load-account-by-token.ts
@@ -0,0 +1,6 @@
+import { AccountType } from "../../utils/enums";
+import { AccountModel } from "../models/account";
+
+export interface LoadAccountByToken{
+    loadByToken: (token: string, role?: AccountType) => Promise<AccountModel | null>
+}
