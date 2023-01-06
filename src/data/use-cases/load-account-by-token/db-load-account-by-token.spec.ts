8cb551f2bb5e60bfeb94d0b509b4269edc92b70e
hotequil <hotequil@protonmail.com>
2023-01-06 20:38:30 -0300

    test: validate if Decrypter return null then loadByToken returns null

--- src/data/use-cases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/use-cases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -1,15 +1,18 @@
 import { DbLoadAccountByToken } from "./db-load-account-by-token";
 import { Decrypter } from "./db-load-account-by-token-protocols";
+import { AccountType } from "../../../utils/enums";
 
 const FAKE_TOKEN = "token"
+const DECRYPT_RESULT = "result"
+const ROLE = AccountType.USER
 let db: DbLoadAccountByToken
 let decrypter: Decrypter
 
 class DecrypterStub implements Decrypter{
-    async decrypt(value: string): Promise<string> {
+    async decrypt(value: string): Promise<string | null> {
         console.log(value)
 
-        return await new Promise(resolve => resolve("result"));
+        return await new Promise(resolve => resolve(DECRYPT_RESULT));
     }
 }
 
@@ -22,8 +25,16 @@ describe(DbLoadAccountByToken.name, () => {
     it("Should call Decrypter with correct values", async () => {
         const decrypterSpy = jest.spyOn(decrypter, "decrypt")
 
-        await db.loadByToken(FAKE_TOKEN)
+        await db.loadByToken(FAKE_TOKEN, ROLE)
 
         expect(decrypterSpy).toHaveBeenCalledWith(FAKE_TOKEN)
+    })
+
+    it("Should return null if Decrypter returns null", async () => {
+        jest.spyOn(decrypter, "decrypt").mockReturnValueOnce(new Promise(resolve => resolve(null)))
+
+        const account = await db.loadByToken(FAKE_TOKEN, ROLE)
+
+        expect(account).toBeNull()
     })
 })
