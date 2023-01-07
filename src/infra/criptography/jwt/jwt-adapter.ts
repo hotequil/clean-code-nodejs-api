9457d438235ca8fd516b5d8eb4d08e63c06aca02
hotequil <hotequil@protonmail.com>
2023-01-07 10:39:13 -0300

    feat: created method decrypt and used verify of JWT

--- src/infra/criptography/jwt/jwt-adapter.ts
+++ src/infra/criptography/jwt/jwt-adapter.ts
@@ -1,5 +1,4 @@
 import jwt from "jsonwebtoken";
-
 import { Encrypter } from "../../../data/protocols/criptography/encrypter";
 import { Decrypter } from "../../../data/protocols/criptography/decrypter";
 
