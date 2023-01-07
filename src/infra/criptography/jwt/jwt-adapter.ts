4a7793a824c41a93555b2183f8e3f953a917f599
hotequil <hotequil@protonmail.com>
2023-01-07 10:38:05 -0300

    feat: created method decrypt and used verify of JWT

--- src/infra/criptography/jwt/jwt-adapter.ts
+++ src/infra/criptography/jwt/jwt-adapter.ts
@@ -1,11 +1,16 @@
 import jwt from "jsonwebtoken";
 
 import { Encrypter } from "../../../data/protocols/criptography/encrypter";
+import { Decrypter } from "../../../data/protocols/criptography/decrypter";
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
     constructor (private readonly secret: string) {}
 
     async encrypt (value: string): Promise<string> {
         return await new Promise(resolve => resolve(jwt.sign({ id: value }, this.secret)));
+    }
+
+    async decrypt(token: string): Promise<string | null> {
+        return await new Promise(resolve => resolve(jwt.verify(token, this.secret) as (string | null)));
     }
 }
