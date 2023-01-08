2363e802a601772403d6dfd9ec571401ceb66c6f
hotequil <hotequil@protonmail.com>
2023-01-08 19:49:00 -0300

    test: verified if POST in /api/surveys with a valid accessToken is correct

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -1,11 +1,11 @@
 import request from "supertest";
 import StatusCode from "status-code-enum";
-import { Collection } from "mongodb";
 import app from "../config/app";
 import { MongodbHelper } from "../../infra/db/mongodb/helpers/mongodb-helper";
 import { AddSurveyModel } from "../../domain/use-cases/add-survey";
-
-let collection: Collection;
+import { sign } from "jsonwebtoken";
+import env from "../config/env";
+import { AccountType, Header } from "../../utils/enums";
 
 const makeAddSurveyModel = (): AddSurveyModel => ({
     question: "question",
@@ -16,15 +16,25 @@ describe("SurveyRoutes", () => {
     beforeAll(async () => await MongodbHelper.connect());
     afterAll(async () => await MongodbHelper.disconnect());
 
-    beforeEach(async () => {
-        collection = await MongodbHelper.collection("surveys");
-
-        await collection.deleteMany({});
-    });
-
     it(`Should return code ${StatusCode.ClientErrorForbidden} when POST in /api/surveys was called without accessToken`, async () => {
         await request(app).post("/api/surveys")
                           .send(makeAddSurveyModel())
                           .expect(StatusCode.ClientErrorForbidden);
     });
+
+    it(`Should return code ${StatusCode.SuccessNoContent} when POST in /api/surveys was called with a valid accessToken`, async () => {
+        const accountsCollection = await MongodbHelper.collection("accounts");
+
+        await accountsCollection.deleteMany({});
+
+        const { insertedId } = await accountsCollection.insertOne({ name: "joao", email: "joao@gmail.com", password: "12345678", role: AccountType.ADMIN })
+        const accessToken = sign({ id: insertedId }, env.JWT_SECRET)
+
+        await accountsCollection.updateOne({ _id: insertedId }, { $set: { accessToken } })
+
+        await request(app).post("/api/surveys")
+                          .set(Header.X_ACCESS_TOKEN, accessToken)
+                          .send(makeAddSurveyModel())
+                          .expect(StatusCode.SuccessNoContent)
+    })
 });
