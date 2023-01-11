cff0234c413fcfb6d9b96eefda2dcc80b52132bf
hotequil <hotequil@protonmail.com>
2023-01-10 21:15:46 -0300

    feat: done loadAll in SurveyMongoRepository

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,13 +1,22 @@
 import { AddSurveyRepository } from "../../../../data/protocols/db/survey/add-survey-repository";
 import { AddSurveyModel } from "../../../../domain/use-cases/add-survey";
 import { MongodbHelper } from "../helpers/mongodb-helper";
+import { LoadSurveysRepository } from "../../../../data/protocols/db/survey/load-surveys-repository";
+import { SurveyModel, SurveysModel } from "../../../../domain/models/survey";
+import { Collection } from "mongodb";
 
-export class SurveyMongoRepository implements AddSurveyRepository{
+export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository{
     async add(model: AddSurveyModel): Promise<null>{
         const collection = await MongodbHelper.collection("surveys")
 
         await collection.insertOne(model)
 
         return null
+    }
+
+    async loadAll(): Promise<SurveysModel> {
+        const collection = await MongodbHelper.collection("surveys") as unknown as Collection<SurveyModel>
+
+        return await collection.find().toArray();
     }
 }
