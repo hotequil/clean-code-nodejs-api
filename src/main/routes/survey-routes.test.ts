84444a885545b64f51c1bf35c06f2deca4b1d2e6
hotequil <hotequil@protonmail.com>
2023-01-11 19:47:19 -0300

    refactor: added a describe for POST in /api/surveys

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -17,25 +17,27 @@ describe("SurveyRoutes", () => {
     beforeAll(async () => await MongodbHelper.connect());
     afterAll(async () => await MongodbHelper.disconnect());
 
-    it(`Should return code ${StatusCode.ClientErrorForbidden} when POST in /api/surveys was called without accessToken`, async () => {
-        await request(app).post("/api/surveys")
-                          .send(makeAddSurveyModel())
-                          .expect(StatusCode.ClientErrorForbidden);
-    });
+    describe("POST: /api/surveys", () => {
+        it(`Should return code ${StatusCode.ClientErrorForbidden} when POST in /api/surveys was called without accessToken`, async () => {
+            await request(app).post("/api/surveys")
+                .send(makeAddSurveyModel())
+                .expect(StatusCode.ClientErrorForbidden);
+        });
 
-    it(`Should return code ${StatusCode.SuccessNoContent} when POST in /api/surveys was called with a valid accessToken`, async () => {
-        const accountsCollection = await MongodbHelper.collection("accounts");
+        it(`Should return code ${StatusCode.SuccessNoContent} when POST in /api/surveys was called with a valid accessToken`, async () => {
+            const accountsCollection = await MongodbHelper.collection("accounts");
 
-        await accountsCollection.deleteMany({});
+            await accountsCollection.deleteMany({});
 
-        const { insertedId } = await accountsCollection.insertOne({ name: "joao", email: "joao@gmail.com", password: "12345678", role: AccountType.ADMIN })
-        const accessToken = sign({ id: insertedId }, env.JWT_SECRET)
+            const { insertedId } = await accountsCollection.insertOne({ name: "joao", email: "joao@gmail.com", password: "12345678", role: AccountType.ADMIN })
+            const accessToken = sign({ id: insertedId }, env.JWT_SECRET)
 
-        await accountsCollection.updateOne({ _id: insertedId }, { $set: { accessToken } })
+            await accountsCollection.updateOne({ _id: insertedId }, { $set: { accessToken } })
 
-        await request(app).post("/api/surveys")
-                          .set(Header.X_ACCESS_TOKEN, accessToken)
-                          .send(makeAddSurveyModel())
-                          .expect(StatusCode.SuccessNoContent)
+            await request(app).post("/api/surveys")
+                .set(Header.X_ACCESS_TOKEN, accessToken)
+                .send(makeAddSurveyModel())
+                .expect(StatusCode.SuccessNoContent)
+        })
     })
 });
