cb80b72cce34a1cc3b9c424f0155af13ce010443
hotequil <hotequil@protonmail.com>
2023-01-14 14:09:02 -0300

    refactor: created protocols file for DbLoadSurveyById

---
+++ src/data/use-cases/load-survey-by-id/db-load-survey-by-id-protocols.ts
@@ -0,0 +1,4 @@
+export * from "@/data/protocols/db/survey/load-survey-by-id-repository";
+export * from "@/domain/models/survey";
+export * from "@/domain/use-cases/load-survey-by-id";
+export * from "@/data/use-cases/load-survey-by-id/db-load-survey-by-id";
--- src/data/use-cases/load-survey-by-id/db-load-survey-by-id.spec.ts
+++ src/data/use-cases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -1,6 +1,4 @@
-import { LoadSurveyByIdRepository } from "@/data/protocols/db/survey/load-survey-by-id-repository";
-import { DbLoadSurveyById } from "@/data/use-cases/load-survey-by-id/db-load-survey-by-id";
-import { SurveyModel } from "@/domain/models/survey";
+import { SurveyModel, DbLoadSurveyById, LoadSurveyByIdRepository } from "./db-load-survey-by-id-protocols";
 import * as MockDate from "mockdate";
 
 let dbLoadSurveyById: DbLoadSurveyById
