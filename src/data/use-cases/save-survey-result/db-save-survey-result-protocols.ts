57ebd19d69eea9c3ff2fa6ac79d4f529515b91c9
hotequil <hotequil@protonmail.com>
2023-01-14 15:00:08 -0300

    feat: created DbSaveSurveyResult with save method and its protocols

---
+++ src/data/use-cases/save-survey-result/db-save-survey-result-protocols.ts
@@ -0,0 +1,4 @@
+export * from "@/domain/use-cases/save-survey-result";
+export * from "@/domain/models/survey-result";
+export * from "@/data/use-cases/save-survey-result/db-save-survey-result"
+export * from "@/data/protocols/db/survey/save-survey-result-repository"
