420c3a99506d4e8a63a05f8e450e998241248a8a
hotequil <hotequil@protonmail.com>
2023-01-16 20:55:31 -0300

    feat: requested loadById if there is no survey

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -1,13 +1,15 @@
 import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from "./save-survey-result-protocols";
-import { success } from "@/presentation/helpers/http-helper";
+import { forbidden, success } from "@/presentation/helpers/http-helper";
+import { InvalidParamsError } from "@/presentation/errors";
 
 export class SaveSurveyResultController implements Controller{
     constructor(private readonly loadSurveyById: LoadSurveyById){}
 
     async handle(request: HttpRequest<any, { surveyId: string }>): Promise<HttpResponse>{
         const surveyId = request?.params?.surveyId as string
+        const survey = await this.loadSurveyById.loadById(surveyId)
 
-        await this.loadSurveyById.loadById(surveyId)
+        if (!survey) return forbidden(new InvalidParamsError("surveyId"))
 
         return success(null);
     }
