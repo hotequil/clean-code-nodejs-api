4300e7589e620dc95b292ef5331674973014900e
hotequil <hotequil@protonmail.com>
2023-01-17 19:53:13 -0300

    feat: validated if hasn't answer provided in SaveSurveyResultController

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller.ts
@@ -5,12 +5,18 @@ import { InvalidParamsError } from "@/presentation/errors";
 export class SaveSurveyResultController implements Controller{
     constructor(private readonly loadSurveyById: LoadSurveyById){}
 
-    async handle(request: HttpRequest<any, { surveyId: string }>): Promise<HttpResponse>{
+    async handle(request: HttpRequest<{ answer: string }, { surveyId: string }>): Promise<HttpResponse>{
         try{
             const surveyId = request?.params?.surveyId as string
             const survey = await this.loadSurveyById.loadById(surveyId)
 
             if (!survey) return forbidden(new InvalidParamsError("surveyId"))
+
+            const answers = survey.answers.map(({ answer }) => answer)
+            const answer = request?.body?.answer as string
+            const hasAnswer = answers.includes(answer)
+
+            if(!hasAnswer) return forbidden(new InvalidParamsError("answer"))
 
             return success(null);
         } catch(error){
