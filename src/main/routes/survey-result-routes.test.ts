dc822f34f5ad45776c1fefdc920a8eb124a7ab09
hotequil <hotequil@protonmail.com>
2023-01-18 21:02:31 -0300

    test: verified if request (PUT) in /api/surveys/:surveyId/results with a valid token gives 200

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -2,8 +2,40 @@ import request from "supertest";
 import StatusCode from "status-code-enum";
 import app from "../config/app";
 import { MongodbHelper } from "@/infra/db/mongodb/helpers/mongodb-helper";
+import { Header } from "@/utils/enums";
+import { sign } from "jsonwebtoken";
+import env from "@/main/config/env";
 
-const makeFakeData = (): { answer: string } => ({ answer: "answer" })
+const ANSWER = "answer"
+
+const makeFakeData = (): { answer: string } => ({ answer: ANSWER })
+
+const makeAccessToken = async (): Promise<string> => {
+    const collection = await MongodbHelper.collection("accounts");
+
+    await collection.deleteMany({});
+
+    const { insertedId: id } = await collection.insertOne({ name: "user", email: "user@email.com", password: "user1234" })
+    const accessToken = sign({ id }, env.JWT_SECRET)
+
+    await collection.updateOne({ _id: id }, { $set: { accessToken } })
+
+    return accessToken
+}
+
+const makeSurveyId = async (): Promise<string> => {
+    const collection = await MongodbHelper.collection("surveys")
+
+    await collection.deleteMany({})
+
+    const { insertedId } = await collection.insertOne({
+        question: "question",
+        answers: [{ answer: ANSWER, image: "image" }],
+        date: new Date(),
+    })
+
+    return insertedId.toJSON()
+}
 
 describe("SurveyResultRoutes", () => {
     beforeAll(async () => {
@@ -19,6 +51,13 @@ describe("SurveyResultRoutes", () => {
             await request(app).put("/api/surveys/1/results")
                               .send(makeFakeData())
                               .expect(StatusCode.ClientErrorForbidden);
+        })
+
+        it(`Should return code ${StatusCode.SuccessOK} when PUT in /api/surveys/:surveyId/results was called with a valid accessToken`, async () => {
+            await request(app).put(`/api/surveys/${await makeSurveyId()}/results`)
+                              .set(Header.X_ACCESS_TOKEN, await makeAccessToken())
+                              .send(makeFakeData())
+                              .expect(StatusCode.SuccessOK);
         })
     })
 });
