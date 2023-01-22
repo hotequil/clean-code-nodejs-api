b5afc05f626733a953f4803ecf331e3b073b733c
hotequil <hotequil@protonmail.com>
2023-01-22 20:39:28 -0300

    feat: added swagger api

---
+++ src/main/docs/index.ts
@@ -0,0 +1,11 @@
+// @ts-ignore
+import { version, description, name } from "../../../package.json"
+
+export default {
+    openapi: "3.0.0",
+    info: {
+        title: name,
+        description,
+        version,
+    }
+}
