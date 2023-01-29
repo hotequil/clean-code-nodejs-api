5a0ff23021a38707f0c10794ec4fce3cdb3cc706
hotequil <hotequil@protonmail.com>
2023-01-29 14:43:14 -0300

    refactor: added ignore in jest files

--- jest-integration.config.ts
+++ jest-integration.config.ts
@@ -1,6 +1,6 @@
-import { Config } from "@jest/types";
-
+// @ts-ignore
 import { default as config } from "./jest.config";
+import { Config } from "@jest/types";
 import { copy } from "./src/presentation/helpers/manipulator-helper";
 
 const integrationConfig = copy<Config.InitialOptions>(config);
