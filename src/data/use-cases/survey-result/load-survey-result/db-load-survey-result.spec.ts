2dc438faa41a94264da680829254e1776d84140e
hotequil <hotequil@protonmail.com>
2023-01-31 20:55:21 -0300

    test: verified if load returns a survey result model on success

--- src/data/use-cases/survey-result/load-survey-result/db-load-survey-result.spec.ts
+++ src/data/use-cases/survey-result/load-survey-result/db-load-survey-result.spec.ts
@@ -1,12 +1,16 @@
 import { DbLoadSurveyResult } from "./db-load-survey-result";
 import { LoadSurveyResultRepository } from "./db-load-survey-result-protocols";
-import { mockLoadSurveyResultRepository, throwError } from "@/utils/tests";
+import { mockLoadSurveyResultRepository, mockSurveyResultModel, throwError } from "@/utils/tests";
+import MockDate from "mockdate";
 
 let db: DbLoadSurveyResult
 let loadSurveyResultRepositoryStub: LoadSurveyResultRepository
 const SURVEY_ID = "survey-id"
 
 describe(DbLoadSurveyResult.name, () => {
+    beforeAll(() => MockDate.set(new Date()))
+    afterAll(() => MockDate.reset())
+
     beforeEach(() => {
         loadSurveyResultRepositoryStub = mockLoadSurveyResultRepository()
         db = new DbLoadSurveyResult(loadSurveyResultRepositoryStub)
@@ -26,5 +30,11 @@ describe(DbLoadSurveyResult.name, () => {
         const promise = db.load(SURVEY_ID)
 
         await expect(promise).rejects.toThrow()
+    })
+
+    it("Should return survey result model when load was called with success", async () => {
+        const result = await db.load(SURVEY_ID)
+
+        expect(result).toEqual(mockSurveyResultModel(SURVEY_ID))
     })
 })
