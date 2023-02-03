310b26828e0bc39c2292680f2952441cc8324b69
hotequil <hotequil@protonmail.com>
2023-02-02 21:11:41 -0300

    docs: removed return from save survey result request

--- src/main/docs/paths/survey-result/save-survey-result-path.ts
+++ src/main/docs/paths/survey-result/save-survey-result-path.ts
@@ -33,14 +33,7 @@ export const saveSurveyResultPath = {
         },
         responses: {
             [StatusCode.SuccessOK]: {
-                description: "Ok",
-                content: {
-                    "application/json": {
-                        schema: {
-                            $ref: "#/schemas/surveyResult"
-                        }
-                    }
-                }
+                description: "Ok"
             },
             [StatusCode.ClientErrorForbidden]: {
                 $ref: "#/components/forbidden"
