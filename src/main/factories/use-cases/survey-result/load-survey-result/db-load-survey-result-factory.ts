3ba053f232d4e292a05ed7e7dd49389d27d45257
hotequil <hotequil@protonmail.com>
2023-02-04 10:00:57 -0300

    feat: created makeLoadSurveyResultController

---
+++ src/main/factories/use-cases/survey-result/load-survey-result/db-load-survey-result-factory.ts
@@ -0,0 +1,7 @@
+import { SurveyResultMongoRepository } from "@/infra/db/mongodb/survey-result/survey-result-mongo-repository";
+import { LoadSurveyResult } from "@/domain/use-cases/survey-result/load-survey-result";
+import { DbLoadSurveyResult } from "@/data/use-cases/survey-result/load-survey-result/db-load-survey-result";
+import { SurveyMongoRepository } from "@/infra/db/mongodb/survey/survey-mongo-repository";
+
+export const makeDbLoadSurveyResult = (): LoadSurveyResult =>
+    new DbLoadSurveyResult(new SurveyResultMongoRepository(), new SurveyMongoRepository())
