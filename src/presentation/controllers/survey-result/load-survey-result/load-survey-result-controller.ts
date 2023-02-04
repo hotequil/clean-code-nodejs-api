bea2ae74dd68ad117c6cf9819b8e8eb3b255b570
hotequil <hotequil@protonmail.com>
2023-02-03 21:18:36 -0300

    feat: added try and catch to return server error

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller.ts
@@ -1,16 +1,20 @@
 import { Controller, HttpRequest, HttpResponse, LoadSurveyById } from "./load-survey-result-protocols";
-import { forbidden, success } from "@/presentation/helpers/http-helper";
+import { forbidden, serverError, success } from "@/presentation/helpers/http-helper";
 import { InvalidParamsError } from "@/presentation/errors";
 
 export class LoadSurveyResultController implements Controller{
     constructor(private readonly loadSurveyById: LoadSurveyById){}
 
     async handle(request: HttpRequest<any, { surveyId: string }>): Promise<HttpResponse> {
-        const surveyId = request.params?.surveyId as string
-        const survey = await this.loadSurveyById.loadById(surveyId)
+        try{
+            const surveyId = request.params?.surveyId as string
+            const survey = await this.loadSurveyById.loadById(surveyId)
 
-        if(!survey) return forbidden(new InvalidParamsError("surveyId"))
+            if(!survey) return forbidden(new InvalidParamsError("surveyId"))
 
-        return success(null);
+            return success(null);
+        } catch (error) {
+            return serverError(error as Error)
+        }
     }
 }
