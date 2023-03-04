1abc562b297ee0be0302d6a5ce2b883a6427d98d
hotequil <hotequil@protonmail.com>
2023-03-04 11:36:39 -0300

    feat: added images and created file to serve static files on server

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,9 +2,11 @@ import express from "express";
 import middlewares from "./middlewares";
 import routes from "./routes";
 import swagger from "@/main/config/swagger";
+import staticFiles from "@/main/config/static-files";
 
 const app = express();
 
+staticFiles(app)
 swagger(app)
 middlewares(app);
 routes(app);
