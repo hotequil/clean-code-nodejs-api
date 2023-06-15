bcc194c6d5f7f399cf81db7f126f551711a41eda
hotequil <hotequil@protonmail.com>
2023-06-15 19:46:27 -0300

    feat: done signUp query

--- src/main/graphql/resolvers/account.ts
+++ src/main/graphql/resolvers/account.ts
@@ -1,15 +1,19 @@
 import { makeLoginController } from "@/main/factories/controllers/account/login/login-controller-factory";
 import { adaptResolver } from "@/main/adapters/apollo-server-resolver-adapter";
+import { makeSignUpController } from "@/main/factories/controllers/account/sign-up/sign-up-controller-factory";
+
+const convertResponseToAccount = ({ token }: any): { accessToken: string, name: string } => ({
+    accessToken: token.token,
+    name: token.name,
+})
 
 export default {
     Query: {
-        login: async (parent: any, args: any) => {
-            const { token } = await adaptResolver(makeLoginController(), args)
-
-            return {
-                accessToken: token.token,
-                name: token.name,
-            }
-        }
+        login: async (parent: any, args: any) =>
+            convertResponseToAccount(await adaptResolver(makeLoginController(), args))
+    },
+    Mutation: {
+        signUp: async (parent: any, args: any) =>
+            convertResponseToAccount(await adaptResolver(makeSignUpController(), args))
     }
 }
