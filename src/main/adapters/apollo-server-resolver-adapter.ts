6eebb7d1f31eb19c2249b018694e89e29a7a382c
hotequil <hotequil@protonmail.com>
2023-06-15 08:55:57 -0300

    feat: added plugin for change status code on GraphQL error

--- src/main/adapters/apollo-server-resolver-adapter.ts
+++ src/main/adapters/apollo-server-resolver-adapter.ts
@@ -1,7 +1,17 @@
 import { Controller } from "@/presentation/protocols";
+import { ApolloError, AuthenticationError, ForbiddenError, UserInputError } from "apollo-server-express";
+import StatusCode from "status-code-enum";
 
 export const adaptResolver = async(controller: Controller, args: any): Promise<any> => {
-    const { body } = await controller.handle(args)
+    const { body, statusCode } = await controller.handle(args)
 
-    return body
+    switch (statusCode) {
+        case StatusCode.SuccessOK:
+        case StatusCode.SuccessCreated:
+        case StatusCode.SuccessNoContent: return body
+        case StatusCode.ClientErrorBadRequest: throw new UserInputError(body.message)
+        case StatusCode.ClientErrorUnauthorized: throw new AuthenticationError(body.message)
+        case StatusCode.ClientErrorForbidden: throw new ForbiddenError(body.message)
+        default: throw new ApolloError(body.message)
+    }
 }
--- src/main/config/apollo-server.ts
+++ src/main/config/apollo-server.ts
@@ -2,9 +2,37 @@ import { Express } from "express";
 import { ApolloServer } from "apollo-server-express";
 import typeDefs from "@/main/graphql/type-defs";
 import resolvers from "@/main/graphql/resolvers";
+import { GraphQLError } from "graphql";
+import StatusCode from "status-code-enum";
+
+type GraphQLErrors = readonly GraphQLError[]
+
+const hasGraphQLError = (errorName: string, errors: GraphQLErrors): boolean =>
+    !!errors?.some(({ name, originalError }) => name === errorName || originalError?.name === errorName)
+
+const applyGraphQLErrorResponse = (statusCode: StatusCode, response: any): void => {
+    response.data = null
+    response.http.status = statusCode
+}
 
 export default async (app: Express): Promise<void> => {
-    const apolloServer = new ApolloServer({ resolvers, typeDefs })
+    const apolloServer = new ApolloServer({
+        resolvers,
+        typeDefs,
+        plugins: [
+            {
+                // @ts-ignore
+                requestDidStart: () => ({
+                    willSendResponse: ({ response, errors }: { response: any, errors: GraphQLErrors }) => {
+                        if(hasGraphQLError("UserInputError", errors)) applyGraphQLErrorResponse(StatusCode.ClientErrorBadRequest, response)
+                        else if(hasGraphQLError("AuthenticationError", errors)) applyGraphQLErrorResponse(StatusCode.ClientErrorUnauthorized, response)
+                        else if(hasGraphQLError("ForbiddenError", errors)) applyGraphQLErrorResponse(StatusCode.ClientErrorForbidden, response)
+                        else if(hasGraphQLError("ApolloError", errors)) applyGraphQLErrorResponse(StatusCode.ServerErrorInternal, response)
+                    }
+                })
+            }
+        ]
+    })
 
     await apolloServer.start();
 
